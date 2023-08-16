583e9db92f62169fddf940ddf029e4497925a8f9
Kim Hyeong Kyu <hyongkui@nate.com>
2023-08-17 00:26:34 +0900

    리스트 형태로 변환

--- crawling/fmkorea.py
+++ crawling/fmkorea.py
@@ -18,3 +18,23 @@ def fmkorea():
             print(price.get_text().strip())
             print(deliver.get_text().strip())
             print('---------------------------')
+
+
+def fmkorea_list():
+    url = requests.get('https://www.fmkorea.com/hotdeal')
+    if url.status_code == 200:
+        html = url.text
+        soup = BeautifulSoup(html, 'html.parser')
+        titles = soup.select('div > div > div > ul > li.li')
+        result_list = []
+        for title in titles:
+            result_dict = \
+                {
+                    'name': title.select_one('div > h3 > a').get_text().strip(),
+                    'shop': title.select_one('div > div > span:nth-child(1)').get_text().strip(),
+                    'price': title.select_one('div > div > span:nth-child(2)').get_text().strip(),
+                    'deliver': title.select_one('div > div > span:nth-child(3)').get_text().strip()
+                }
+            result_list.append(result_dict)
+
+        return result_list
