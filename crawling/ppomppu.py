f4d1cbcd5adc10f822c87623659b45b220fda848
Kim Hyeong Kyu <hyongkui@nate.com>
2023-08-24 23:53:19 +0900

    뽐뿌 데이터 리스트로 변경

--- crawling/ppomppu.py
+++ crawling/ppomppu.py
@@ -18,3 +18,31 @@ def ppomppu():
                 else:
                     print("없음")
                 print('------------------------------')
+
+
+def ppomppu_list():
+    url = requests.get('https://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu')
+    if url.status_code == 200:
+        html = url.text
+        soup = BeautifulSoup(html, 'html.parser')
+        titles = soup.select('tr td:nth-child(2) > div')
+        result_list = []
+        for title in titles[3:42]:
+            name = title.select_one('a > font.list_title')
+            category = title.select_one('span[style="color:#999;font-size:11px;"]')
+            if name is not None:
+                if category is not None:
+                    result_dict = \
+                        {
+                            'name': name.get_text().strip(),
+                            'category': category.get_text().strip()
+                        }
+                else:
+                    result_dict = \
+                        {
+                            'name': name.get_text().strip(),
+                            'category': '없음'
+                        }
+                result_list.append(result_dict)
+
+        return result_list
