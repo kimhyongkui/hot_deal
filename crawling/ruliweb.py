b3eeafd614681289f6d0cd8fed61b4072230d23e
Kim Hyeong Kyu <hyongkui@nate.com>
2023-08-25 23:18:30 +0900

    루리웹 리스트 추가

--- crawling/ruliweb.py
+++ crawling/ruliweb.py
@@ -11,3 +11,19 @@ def ruliweb():
         for title in board:
             print(title.text.strip())
             print('------------------------------')
+
+def ruliweb_list():
+    url = requests.get('https://bbs.ruliweb.com/market/board/1020')
+    if url.status_code == 200:
+        html = url.text
+        soup = BeautifulSoup(html, 'html.parser')
+        board = soup.select('tbody > tr.table_body.blocktarget > td > div > a.deco')
+        result_list = []
+        for title in board:
+            result_dict = \
+                {
+                    'name': title.text.strip()
+                }
+            result_list.append(result_dict)
+
+        return result_list
