bfd91bbe5c556bdd438a77f173d0c98b72996891
Kim Hyeong Kyu <hyongkui@nate.com>
2023-09-28 10:41:20 +0900

    테이블을 나타내는 모델 생성

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 
-class ListData(models.Model):
+class Ruliweb(models.Model):
     name = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10)
 
     class Meta:
         app_label = "hot_deal"
