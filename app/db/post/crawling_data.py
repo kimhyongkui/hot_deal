c4c60f16bad7b1df4dda68f8728a1b0ed35e3374
Kim Hyeong Kyu <hyongkui@nate.com>
2023-10-03 21:57:40 +0900

    크롤링한 데이터 db에 저장

--- app/db/post/crawling_data.py
+++ app/db/post/crawling_data.py
@@ -1,14 +1,34 @@
 from app.models import Fmkorea, Ppomppu, Ruliweb  # 필요한 모델 클래스를 가져옵니다.
 from app.crawling.fmkorea import fmkorea_list
+from app.crawling.ppomppu import ppomppu_list
+from app.crawling.ruliweb import ruliweb_list
 
 
-def save_data_to_database():
+def save_data_fmkorea():
     for data in fmkorea_list:
         fmkorea_obj = Fmkorea(
             name=data['name'],
             category=data['category'],
             price=data['price']
         )
-        fmkorea_obj.save()  # 데이터베이스에 저장합니다.
+        fmkorea_obj.save()
 
 
+def save_data_ppomppu():
+    for data in ppomppu_list:
+        ppomppu_obj = Ppomppu(
+            name=data['name'],
+            category=data['category'],
+            price=data['price']
+        )
+        ppomppu_obj.save()
+
+
+def save_data_ruliweb():
+    for data in ruliweb_list:
+        ruliweb_obj = Ruliweb(
+            name=data['name'],
+            category=data['category'],
+            price=data['price']
+        )
+        ruliweb_obj.save()
