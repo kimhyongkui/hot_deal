64422a9c27e885b559bda6df9b844060c4e8d27f
Kim Hyeong Kyu <hyongkui@nate.com>
2023-10-16 17:33:12 +0900

    INT 유형 매핑

--- app/models.py
+++ app/models.py
@@ -2,25 +2,25 @@ from django.db import models
 
 
 class Fmkorea(models.Model):
-    name = models.CharField(max_length=255)
-    shop = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10)
-    deliver = models.CharField(max_length=255)
+    name = models.CharField(max_length=45)
+    shop = models.CharField(max_length=45)
+    price = models.IntegerField()
+    deliver = models.CharField(max_length=45)
 
     class Meta:
         app_label = "hot_deal"
 
 
 class Ppomppu(models.Model):
-    name = models.CharField(max_length=255)
-    category = models.CharField(max_length=255)
+    name = models.CharField(max_length=45)
+    category = models.CharField(max_length=45)
 
     class Meta:
         app_label = "hot_deal"
 
 
 class Ruliweb(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=45)
 
     class Meta:
         app_label = "hot_deal"
