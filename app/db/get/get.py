e9c26c9d9ac4a071add3ad1edbfdf2f60447a374
Kim Hyeong Kyu <hyongkui@nate.com>
2023-10-21 23:55:33 +0900

    get기능

--- app/db/get/get.py
+++ app/db/get/get.py
@@ -1,5 +1,14 @@
-from db.models import Fmkorea
+from app.models import Fmkorea, Ppomppu, Ruliweb
 
 
 def get_fmkorea_data():
     result = Fmkorea.objects.all()
+    return result
+
+def get_ppomppu_data():
+    result = Ppomppu.objects.all()
+    return result
+
+def get_ruliweb_data():
+    result = Ruliweb.objects.all()
+    return result
