050f0e055303078fab05e6a873198f3254e5ae34
Kim Hyeong Kyu <hyongkui@nate.com>
2023-11-01 23:57:35 +0900

    views.py 만들어보기

--- crawler/views.py
+++ crawler/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hot_deal.settings")
+
+import django
+
+django.setup()
+
+from django.shortcuts import render
+from crawler.models import Fmkorea
+
+def Fmkorea_list(request):
+    tasks = Fmkorea.objects.all()
+    return render(request, 'Fmkorea_list.html', {'tasks': tasks})
+
+def Fmkorea_detail(request, pk):
+    task = Fmkorea.objects.get(id=pk)
+    return render(request, 'Fmkorea_detail.html', {'task': task})
+
+
+print(Fmkorea_list())
