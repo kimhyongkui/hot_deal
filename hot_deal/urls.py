f536ab8c234adab9173c1f23ef7308a9c2884768
Kim Hyeong Kyu <hyongkui@nate.com>
2023-11-02 23:49:27 +0900

    urls.py 작성

--- hot_deal/urls.py
+++ hot_deal/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path
+from crawler import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls)
+    path('fmkorea/', views.fmkorea_list, name='fmkorea_list'),
+    path('fmkorea/<int:pk>/', views.fmkorea_detail, name='fmkorea_detail'),
 ]
