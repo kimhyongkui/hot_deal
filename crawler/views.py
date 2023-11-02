0818055fbd5d473e13617a50d3d81dbba5ff2411
Kim Hyeong Kyu <hyongkui@nate.com>
2023-11-02 23:49:16 +0900

    views.py 작성

--- crawler/views.py
+++ crawler/views.py
@@ -9,13 +9,12 @@ django.setup()
 from django.shortcuts import render
 from crawler.models import Fmkorea
 
-def Fmkorea_list(request):
+
+def fmkorea_list(request):
     tasks = Fmkorea.objects.all()
     return render(request, 'Fmkorea_list.html', {'tasks': tasks})
 
-def Fmkorea_detail(request, pk):
+
+def fmkorea_detail(request, pk):
     task = Fmkorea.objects.get(id=pk)
     return render(request, 'Fmkorea_detail.html', {'task': task})
-
-
-print(Fmkorea_list())
