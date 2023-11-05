c3707612079cc7f3e9c8d32356ba44b1844f01b1
Kim Hyeong Kyu <hyongkui@nate.com>
2023-11-05 23:16:23 +0900

    뽐뿌랑 루리웹 views.py에 추가

--- crawler/views.py
+++ crawler/views.py
@@ -7,7 +7,7 @@ import django
 django.setup()
 
 from django.shortcuts import render
-from crawler.models import Fmkorea
+from crawler.models import Fmkorea, Ppomppu, Ruliweb
 
 
 def fmkorea_list(request):
@@ -18,3 +18,23 @@ def fmkorea_list(request):
 def fmkorea_detail(request, pk):
     task = Fmkorea.objects.get(id=pk)
     return render(request, 'fmkorea_detail.html', {'task': task})
+
+
+def ppomppu_list(request):
+    tasks = Ppomppu.objects.all()
+    return render(request, 'ppomppu_list.html', {'tasks': tasks})
+
+
+def ppomppu_detail(request, pk):
+    task = Ppomppu.objects.get(id=pk)
+    return render(request, 'ppomppu_detail.html', {'task': task})
+
+
+def ruliweb_list(request):
+    tasks = Ruliweb.objects.all()
+    return render(request, 'ruliweb_list.html', {'tasks': tasks})
+
+
+def ruliweb_detail(request, pk):
+    task = Ruliweb.objects.get(id=pk)
+    return render(request, 'ruliweb_detail.html', {'task': task})
