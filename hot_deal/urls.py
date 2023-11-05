82c4e27fbbbfb23a82161ccb3ebc35db6b4e4fcd
Kim Hyeong Kyu <hyongkui@nate.com>
2023-11-05 23:17:14 +0900

    뽐뿌 url추가

--- hot_deal/urls.py
+++ hot_deal/urls.py
@@ -4,4 +4,8 @@ from crawler import views
 urlpatterns = [
     path('fmkorea/', views.fmkorea_list, name='fmkorea_list'),
     path('fmkorea/<int:pk>/', views.fmkorea_detail, name='fmkorea_detail'),
+    path('ppomppu/', views.ppomppu_list, name='ppomppu_list'),
+    path('ppomppu/<int:pk>/', views.ppomppu_detail, name='ppomppu_detail'),
+    path('ruliweb/', views.ruliweb_list, name='ruliweb_list'),
+    path('ruliweb/<int:pk>/', views.ruliweb_detail, name='ruliweb_detail')
 ]
