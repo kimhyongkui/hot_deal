ffe4f95917861888babf2e27f9e766ed5b26d079
Kim Hyeong Kyu <hyongkui@nate.com>
2023-11-16 22:02:54 +0900

    name을 unique key로

--- crawler/models.py
+++ crawler/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Fmkorea(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     shop = models.CharField(max_length=45)
     price = models.CharField(max_length=45)
     deliver = models.CharField(max_length=45)
@@ -12,13 +12,13 @@ class Fmkorea(models.Model):
 
 class Ppomppu(models.Model):
     category = models.CharField(max_length=45)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     date = models.CharField(max_length=45)
     url = models.CharField(max_length=255)
 
 
 class Ruliweb(models.Model):
     category = models.CharField(max_length=45)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     date = models.CharField(max_length=45)
     url = models.CharField(max_length=255)
