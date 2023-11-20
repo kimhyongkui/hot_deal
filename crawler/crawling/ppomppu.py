4ed1cf8bfd925376a3cd6a76fcf4b342e4f4980e
Kim Hyeong Kyu <hyongkui@nate.com>
2023-11-20 22:37:58 +0900

    리스트를 만들때 number를 기준으로 내림차순 정렬

--- crawler/crawling/ppomppu.py
+++ crawler/crawling/ppomppu.py
@@ -51,4 +51,6 @@ def ppomppu_list():
                     }
                 result_list.append(result_dict)
 
+        result_list = sorted(result_list, key=lambda x: x['number'], reverse=True)
+
         return result_list
