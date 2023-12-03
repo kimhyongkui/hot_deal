2cb70612682e3da2f8f229bc7e69d1c4d6dc4888
Kim Hyeong Kyu <hyongkui@nate.com>
2023-12-03 23:31:42 +0900

    디스코드에 원하는 메시지 보내기 성공

--- crawler/notification/discord_noti.py
+++ crawler/notification/discord_noti.py
@@ -1,12 +1,21 @@
 import discord
+from dotenv import load_dotenv
+import os
 
-async def send_discord_notification(token, channel_id, message):
-    client = discord.Client()
+load_dotenv()
+
+
+def send_discord_notification(message):
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+    channel_id = int(os.getenv("APP_ID"))
+    token = os.getenv("BOT_TOKEN")
 
     @client.event
     async def on_ready():
         channel = client.get_channel(channel_id)
         await channel.send(message)
-        await client.close()
+        client.close()
 
     client.run(token)
