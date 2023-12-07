67d5496cc259962c964cb6e44c604b5dd1710210
Kim Hyeong Kyu <hyongkui@nate.com>
2023-12-07 23:26:49 +0900

    디코 클라 생성 종료 반복을 막기 위해 asyncio 사용

--- crawler/notification/discord_noti.py
+++ crawler/notification/discord_noti.py
@@ -1,24 +1,23 @@
 import discord
 from dotenv import load_dotenv
 import os
+import asyncio
 
 load_dotenv()
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
-channel_id = int(os.getenv("APP_ID"))
-token = os.getenv("BOT_TOKEN")
 
+def send_discord_notification(message):
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+    channel_id = int(os.getenv("APP_ID"))
+    token = os.getenv("BOT_TOKEN")
 
-async def send_discord_notification(message):
-    channel = client.get_channel(channel_id)
-    await channel.send(message)
+    @client.event
+    async def on_ready():
+        channel = client.get_channel(channel_id)
+        await channel.send(message)
+        await client.close()
 
-
-@client.event
-async def on_ready():
-    await client.close()
-
-
-client.run(token)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(client.start(token))
