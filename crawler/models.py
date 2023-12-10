adc386c7589fe7f53feeae0b7f4a902b47435daa
Kim Hyeong Kyu <hyongkui@nate.com>
2023-12-10 23:52:42 +0900

    유니크키를 url에 부여(글제목은 중복이 가능함)

--- crawler/models.py
+++ crawler/models.py
@@ -2,25 +2,25 @@ from django.db import models
 
 
 class Fmkorea(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     shop = models.CharField(max_length=45)
     price = models.CharField(max_length=45)
     deliver = models.CharField(max_length=45)
     date = models.CharField(max_length=45)
-    url = models.CharField(max_length=255)
+    url = models.CharField(max_length=255, unique=True)
 
 
 class Ppomppu(models.Model):
     number = models.CharField(max_length=30, primary_key=True)
     category = models.CharField(max_length=45)
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     date = models.CharField(max_length=45)
-    url = models.CharField(max_length=255)
+    url = models.CharField(max_length=255, unique=True)
 
 
 class Ruliweb(models.Model):
     number = models.CharField(max_length=30, primary_key=True)
     category = models.CharField(max_length=45)
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     date = models.CharField(max_length=45)
-    url = models.CharField(max_length=255)
+    url = models.CharField(max_length=255, unique=True)
