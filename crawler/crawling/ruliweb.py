e2225a48efd4f62d5b852710f41cd97e92b36582
Kim Hyeong Kyu <hyongkui@nate.com>
2024-01-17 23:51:35 +0900

    db를 다루는 파일엔 db를 조작하는 코드만

--- crawler/crawling/ruliweb.py
+++ crawler/crawling/ruliweb.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+from crawler.models import Ruliweb
+from crawler.notification.discord_noti import send_discord_notification
 
 
 def ruliweb():
@@ -21,6 +23,33 @@ def ruliweb():
             print(number.text.strip())
             print('------------------------------')
 
+
+# def ruliweb_list():
+#     web_url = requests.get('https://bbs.ruliweb.com/market/board/1020')
+#     if web_url.status_code == 200:
+#         html = web_url.text
+#         soup = BeautifulSoup(html, 'html.parser')
+#         titles = soup.select('tbody > tr.table_body.blocktarget')
+#         result_list = []
+#         for title in titles[4:32]:
+#             category = title.select_one('td.divsn.text_over > a[tabindex="-1"]')
+#             name = title.select_one('td.subject > div > a.deco')
+#             date = title.select_one('td.time')
+#             url = title.select_one('td.subject > div > a.deco').get('href')
+#             number = title.select_one('td.id')
+#             result_dict = \
+#                 {
+#                     'number': number.text.strip(),
+#                     'category': category.text.strip(),
+#                     'name': name.text.strip(),
+#                     'date': date.text.strip(),
+#                     'url': url
+#                 }
+#             result_list.append(result_dict)
+#
+#         result_list = sorted(result_list, key=lambda x: x['number'])
+#
+#         return result_list
 
 def ruliweb_list():
     web_url = requests.get('https://bbs.ruliweb.com/market/board/1020')
@@ -46,5 +75,22 @@ def ruliweb_list():
             result_list.append(result_dict)
 
         result_list = sorted(result_list, key=lambda x: x['number'])
+        new_post_count = 0
+        for data in result_list:
+            check_data = Ruliweb.objects.filter(url=data['url']).first()
+            if check_data:
+                continue
+            ruliweb_obj = Ruliweb(
+                number=data['number'],
+                category=data['category'],
+                name=data['name'],
+                date=data['date'],
+                url=data['url']
+            )
 
-        return result_list
+            result = ruliweb_obj
+            new_post_count += 1
+        if new_post_count >= 0:
+            print(f"{new_posts_count}개 업데이트")
+            message = f"ruliweb : {new_posts_count}개 업데이트"
+            send_discord_notification(message)
--- crawler/db/post/crawling_data.py
+++ crawler/db/post/crawling_data.py
@@ -66,6 +66,28 @@ def save_data_ppomppu():
         send_discord_notification(message)
 
 
+# def save_data_ruliweb():
+#     data_list = ruliweb_list()
+#     new_posts_count = 0
+#     for data in data_list:
+#         check_data = Ruliweb.objects.filter(number=data['number']).first()
+#         if check_data:
+#             continue
+#         ruliweb_obj = Ruliweb(
+#             number=data['number'],
+#             category=data['category'],
+#             name=data['name'],
+#             date=data['date'],
+#             url=data['url']
+#         )
+#         ruliweb_obj.save()
+#         new_posts_count += 1
+#     if new_posts_count >= 0:
+#         print(f"{new_posts_count}개 업데이트")
+#         message = f"ruliweb : {new_posts_count}개 업데이트"
+#         send_discord_notification(message)
+
+
 def save_data_ruliweb():
     data_list = ruliweb_list()
     new_posts_count = 0
