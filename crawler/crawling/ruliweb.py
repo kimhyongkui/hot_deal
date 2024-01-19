d70e89d0bade6b1fbbd743362ec7e59315f2d0fb
Kim Hyeong Kyu <hyongkui@nate.com>
2024-01-20 07:23:21 +0900

    크롤링한 데이터 조작하는 함수 추가

--- crawler/crawling/ruliweb.py
+++ crawler/crawling/ruliweb.py
@@ -12,60 +12,14 @@ from crawler.models import Ruliweb
 from crawler.notification.discord_noti import send_discord_notification
 from crawler.db.post.crawling_data import save_data_ruliweb
 
-def ruliweb():
-    web_url = requests.get('https://bbs.ruliweb.com/market/board/1020')
-    if web_url.status_code == 200:
-        html = web_url.text
-        soup = BeautifulSoup(html, 'html.parser')
-        titles = soup.select('tbody > tr.table_body.blocktarget')
-        for title in titles[4:32]:
-            category = title.select_one('td.divsn.text_over > a[tabindex="-1"]')
-            name = title.select_one('td.subject > div > a.deco')
-            date = title.select_one('td.time')
-            url = title.select_one('td.subject > div > a.deco').get('href')
-            number = title.select_one('td.id')
-            print(category.text.strip())
-            print(name.text.strip())
-            print(date.text.strip())
-            print(url)
-            print(number.text.strip())
-            print('------------------------------')
-
-
-# def ruliweb_list():
-#     web_url = requests.get('https://bbs.ruliweb.com/market/board/1020')
-#     if web_url.status_code == 200:
-#         html = web_url.text
-#         soup = BeautifulSoup(html, 'html.parser')
-#         titles = soup.select('tbody > tr.table_body.blocktarget')
-#         result_list = []
-#         for title in titles[4:32]:
-#             category = title.select_one('td.divsn.text_over > a[tabindex="-1"]')
-#             name = title.select_one('td.subject > div > a.deco')
-#             date = title.select_one('td.time')
-#             url = title.select_one('td.subject > div > a.deco').get('href')
-#             number = title.select_one('td.id')
-#             result_dict = \
-#                 {
-#                     'number': number.text.strip(),
-#                     'category': category.text.strip(),
-#                     'name': name.text.strip(),
-#                     'date': date.text.strip(),
-#                     'url': url
-#                 }
-#             result_list.append(result_dict)
-#
-#         result_list = sorted(result_list, key=lambda x: x['number'])
-#
-#         return result_list
 
 def ruliweb_list():
     web_url = requests.get('https://bbs.ruliweb.com/market/board/1020')
+    result_list = []
     if web_url.status_code == 200:
         html = web_url.text
         soup = BeautifulSoup(html, 'html.parser')
         titles = soup.select('tbody > tr.table_body.blocktarget')
-        result_list = []
         for title in titles[4:32]:
             category = title.select_one('td.divsn.text_over > a[tabindex="-1"]')
             name = title.select_one('td.subject > div > a.deco')
@@ -83,14 +37,27 @@ def ruliweb_list():
             result_list.append(result_dict)
 
         result_list = sorted(result_list, key=lambda x: x['number'])
+
     return result_list
 
 
-def count_list(ruliweb_list):
+def count_list():
     data_list = ruliweb_list()
-    new_list = []
+    new_post_count = 0
+    message_list = []
     for data in data_list:
         check_data = Ruliweb.objects.filter(url=data['url']).first()
         if not check_data:
-            new_list.append(data)
-    return new_list
+            ruliweb_obj = Ruliweb(
+                number=data['number'],
+                category=data['category'],
+                name=data['name'],
+                date=data['date'],
+                url=data['url']
+            )
+            save_data_ruliweb(ruliweb_obj)
+            message_list.append(f"{data['name']}")
+            new_post_count += 1
+    if new_post_count >= 0:
+        message_list.append(f"ruliweb : {new_post_count}개 업데이트")
+        send_discord_notification(message_list)
