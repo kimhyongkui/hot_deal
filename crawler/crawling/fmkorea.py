fcf24397d3c23a58accfce23bdc737995e9768e8
Kim Hyeong Kyu <hyongkui@nate.com>
2024-01-22 23:57:32 +0900

    Fmkorea 크롤링, db 저장 코드 분리

--- crawler/crawling/fmkorea.py
+++ crawler/crawling/fmkorea.py
@@ -1,27 +1,16 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hot_deal.settings.development")
+
+import django
+
+django.setup()
+
 from bs4 import BeautifulSoup
 import requests
-
-
-def fmkorea():
-    web_url = requests.get('https://www.fmkorea.com/hotdeal')
-    if web_url.status_code == 200:
-        html = web_url.text
-        soup = BeautifulSoup(html, 'html.parser')
-        titles = soup.select('div > div > div > ul > li.li')
-        for title in titles:
-            name = title.select_one('div > h3 > a')
-            shop = title.select_one('div > div > span:nth-child(1)')
-            price = title.select_one('div > div > span:nth-child(2)')
-            deliver = title.select_one('div > div > span:nth-child(3)')
-            date = title.select_one('div > div > span.regdate')
-            url = name.get('href')
-            print(name.text.strip())
-            print(shop.text.strip())
-            print(price.text.strip())
-            print(deliver.text.strip())
-            print(date.text.strip())
-            print(f"https://www.fmkorea.com/{url}")
-            print('---------------------------')
+from crawler.models import Fmkorea
+from crawler.notification.discord_noti import send_discord_notification
+from crawler.db.post.crawling_data import save_data_fmkorea
 
 
 def fmkorea_list():
@@ -52,3 +41,26 @@ def fmkorea_list():
         result_list = sorted(result_list, key=lambda x: x['date'])
 
         return result_list
+
+
+def count_list():
+    data_list = fmkorea_list()
+    new_post_count = 0
+    message_list = []
+    for data in data_list:
+        check_data = Fmkorea.objects.filter(url=data['url']).first()
+        if not check_data:
+            fmkorea_obj = Fmkorea(
+                name=data['number'],
+                shop=data['shop'],
+                price=data['price'],
+                deliver=data['deliver'],
+                date=data['date'],
+                url=data['url']
+            )
+            save_data_fmkorea(fmkorea_obj)
+            message_list.append(f"{data['name']}")
+            new_post_count += 1
+    if new_post_count >= 0:
+        message_list.append(f"ruliweb : {new_post_count}개 업데이트")
+        send_discord_notification(message_list)
