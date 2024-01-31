9658df5f2aca4890b2f17e2b5c8588d8b3217b10
Kim Hyeong Kyu <hyongkui@nate.com>
2024-01-31 23:30:58 +0900

    모듈까지 분리해서 테스트

--- crawler/crawling/ruliweb.py
+++ crawler/crawling/ruliweb.py
@@ -52,22 +52,22 @@ def save_ruliweb_list(data):
     save_data(ruliweb_obj)
 
 
-def count_and_notify(new_post_count, message_list):
-    if new_post_count >= 0:
-        message_list.append(f"ruliweb : {new_post_count}개 업데이트")
-        send_discord_notification(message_list)
-
-
-def count_ruliweb_list():
-    data_list = ruliweb_list()
-    new_post_count = 0
-    message_list = []
-
-    for data in data_list:
-        check_data = Ruliweb.objects.filter(url=data['url']).first()
-        if not check_data:
-            save_ruliweb_list(data)
-            message_list.append(f"{data['name']}")
-            new_post_count += 1
-
-    count_and_notify(new_post_count, message_list)
+# def count_and_notify(new_post_count, message_list):
+#     if new_post_count >= 0:
+#         message_list.append(f"ruliweb : {new_post_count}개 업데이트")
+#         send_discord_notification(message_list)
+#
+#
+# def count_ruliweb_list():
+#     data_list = ruliweb_list()
+#     new_post_count = 0
+#     message_list = []
+#
+#     for data in data_list:
+#         check_data = Ruliweb.objects.filter(url=data['url']).first()
+#         if not check_data:
+#             save_ruliweb_list(data)
+#             message_list.append(f"{data['name']}")
+#             new_post_count += 1
+#
+#     count_and_notify(new_post_count, message_list)
