360f5cc6bff8f3e6ef9a29d2be3beb6480b17046
Kim Hyeong Kyu <hyongkui@nate.com>
2024-02-01 23:58:10 +0900

    크롤링할 사이트를 매개변수로 받도록 설정 중

--- crawler/crawling/counting.py
+++ crawler/crawling/counting.py
@@ -11,24 +11,44 @@ from crawler.notification.discord_noti import send_discord_notification
 from crawler.crawling.ruliweb import ruliweb_list, save_ruliweb_list
 
 
-def count_and_notify(new_post_count, message_list):
+# def send_notify(new_post_count, message_list):
+#     if new_post_count >= 0:
+#         message_list.append(f"ruliweb : {new_post_count}개 업데이트")
+#         send_discord_notification(message_list)
+#
+#
+# def count_list():
+#     data_list = ruliweb_list()
+#     new_post_count = 0
+#     message_list = []
+#
+#     for data in data_list:
+#         check_data = Ruliweb.objects.filter(url=data['url']).first()
+#         if not check_data:
+#             save_ruliweb_list(data)
+#             message_list.append(f"{data['name']}")
+#             new_post_count += 1
+#
+#     send_notify(new_post_count, message_list)
+
+def send_notify(site_name, new_post_count, message_list):
     if new_post_count >= 0:
-        message_list.append(f"ruliweb : {new_post_count}개 업데이트")
+        message_list.append(f"{site_name} : {new_post_count}개 업데이트")
         send_discord_notification(message_list)
 
 
-def count_ruliweb_list():
-    data_list = ruliweb_list()
+def count_list(site_name):
+    data_list = f"{site_name}_list()"
     new_post_count = 0
     message_list = []
-
+    table_name = site_name.capitalize()
     for data in data_list:
-        check_data = Ruliweb.objects.filter(url=data['url']).first()
+        check_data = f"{table_name}.objects.filter(url=data['url']).first()"
         if not check_data:
             save_ruliweb_list(data)
             message_list.append(f"{data['name']}")
             new_post_count += 1
 
-    count_and_notify(new_post_count, message_list)
+    send_notify(site_name, new_post_count, message_list)
 
-count_ruliweb_list()
+count_list("ruliweb")
