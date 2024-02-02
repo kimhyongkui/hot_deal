a916377ea9f0da308cf736ee2e9a113f4c27960d
Kim Hyeong Kyu <hyongkui@nate.com>
2024-02-02 23:57:52 +0900

    글로벌 함수를 사용해 매개변수를 사이트 이름으로 수정

--- crawler/crawling/count_and_notify.py
+++ crawler/crawling/count_and_notify.py
@@ -6,30 +6,10 @@ import django
 
 django.setup()
 
-from crawler.models import Ruliweb
 from crawler.notification.discord_noti import send_discord_notification
+from crawler.models import Ruliweb
 from crawler.crawling.ruliweb import ruliweb_list, save_ruliweb_list
 
-
-# def send_notify(new_post_count, message_list):
-#     if new_post_count >= 0:
-#         message_list.append(f"ruliweb : {new_post_count}개 업데이트")
-#         send_discord_notification(message_list)
-#
-#
-# def count_list():
-#     data_list = ruliweb_list()
-#     new_post_count = 0
-#     message_list = []
-#
-#     for data in data_list:
-#         check_data = Ruliweb.objects.filter(url=data['url']).first()
-#         if not check_data:
-#             save_ruliweb_list(data)
-#             message_list.append(f"{data['name']}")
-#             new_post_count += 1
-#
-#     send_notify(new_post_count, message_list)
 
 def send_notify(site_name, new_post_count, message_list):
     if new_post_count >= 0:
@@ -38,17 +18,14 @@ def send_notify(site_name, new_post_count, message_list):
 
 
 def count_list(site_name):
-    data_list = f"{site_name}_list()"
+    data_list = globals()[f"{site_name}_list"]()
     new_post_count = 0
     message_list = []
-    table_name = site_name.capitalize()
     for data in data_list:
-        check_data = f"{table_name}.objects.filter(url=data['url']).first()"
+        check_data = globals()[site_name.capitalize()].objects.filter(url=data['url']).first()
         if not check_data:
-            f"save_{site_name}_list(data)"
+            globals()[f"save_{site_name}_list"](data)
             message_list.append(f"{data['name']}")
             new_post_count += 1
 
     send_notify(site_name, new_post_count, message_list)
-
-count_list("ruliweb")
