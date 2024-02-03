1a74a3c700d8472413a6e669a4b24aa0d953d86c
Kim Hyeong Kyu <hyongkui@nate.com>
2024-02-03 19:18:13 +0900

    schedule함수 수정

--- crawler/management/commands/task.py
+++ crawler/management/commands/task.py
@@ -1,7 +1,5 @@
 from django.core.management.base import BaseCommand
-from crawler.crawling.fmkorea import count_fmkorea_list
-from crawler.crawling.ppomppu import count_ppomppu_list
-from crawler.crawling.ruliweb import count_ruliweb_list
+from crawler.crawling.count_and_notify import count_list
 import schedule
 import time
 
@@ -10,9 +8,9 @@ class Command(BaseCommand):
     help = 'Update data'
 
     def handle(self, *args, **options):
-        schedule.every(1).minute.do(lambda: count_fmkorea_list())
-        schedule.every(1).minute.do(lambda: count_ppomppu_list())
-        schedule.every(10).seconds.do(lambda: count_ruliweb_list())
+        schedule.every(1).minute.do(lambda: count_list("fmkorea"))
+        schedule.every(1).minute.do(lambda: count_list("ppomppu"))
+        schedule.every(1).minute.do(lambda: count_list("ruliweb"))
 
         while True:
             schedule.run_pending()
